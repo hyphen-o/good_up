aa6f507ee27eeb9ba7e720297c0d04867b5d558b
Rika Kumakura <s256088@wakayama-u.ac.jp>
2022-07-13 16:21:40 +0900

    homescreen

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -4,15 +4,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.green[100],
+        backgroundColor: Colors.white,
         // Appbar
         appBar: AppBar(
-          backgroundColor: Colors.green[100],
-          title: Text(
-            'HOME',
-            style: TextStyle(fontSize: 16),
-          ),
+          backgroundColor:Colors.deepOrange,
+          title: Row(children: [
+            Text("ホーム"),
+          ]),
         ),
-        body: Center(child: Icon(Icons.home)));
+        body: Center(
+          child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    // 枠線
+                    border: Border.all(color: Colors.black12, width: 20),
+                    // 角丸
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.all(100),
+                  child: Column(
+                  children: <Widget>[
+                    Text("ただいま",style: TextStyle(fontSize: 50),textAlign: TextAlign.left),
+                    Text('50',style: TextStyle(fontSize: 100,fontWeight: FontWeight.bold)),
+                    Text("ポイント",style: TextStyle(fontSize: 50),textAlign: TextAlign.right),
+                  ],
+                  ),
+                ),
+              ],
+          ),
+
+       ),
+    );
   }
 }
