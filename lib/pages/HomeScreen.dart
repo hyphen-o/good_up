03b8f85383f82e1730cc0412ab1a493f7aece086
Rika Kumakura <s256088@wakayama-u.ac.jp>
2022-07-27 15:24:58 +0900

    homescreen

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -1,40 +1,34 @@
 import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
-  // This widget is the root of your application.
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        // Appbar
+    return DefaultTabController(
+      initialIndex: 0, // 最初に表示するタブ
+      length: 2, // タブの数
+      child: Scaffold(
         appBar: AppBar(
           backgroundColor:Colors.deepOrange,
-          title: Row(children: [
-            Text("ホーム"),
-          ]),
-        ),
-        body: Center(
-          child: Column(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    // 枠線
-                    border: Border.all(color: Colors.black12, width: 20),
-                    // 角丸
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: EdgeInsets.all(100),
-                  child: Column(
-                  children: <Widget>[
-                    Text("ただいま",style: TextStyle(fontSize: 50),textAlign: TextAlign.left),
-                    Text('50',style: TextStyle(fontSize: 100,fontWeight: FontWeight.bold)),
-                    Text("ポイント",style: TextStyle(fontSize: 50),textAlign: TextAlign.right),
-                  ],
-                  ),
-                ),
-              ],
+          title: const Text('Home'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(text: 'ポイント'),
+              Tab(text: 'メッセージ'),
+            ],
           ),
-
-       ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text('ただいま\n５０\nポイント', style: TextStyle(fontSize: 50.0)),
+            ),
+            Center(
+              child: Text('メッセージ', style: TextStyle(fontSize: 50.0)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
